55c36bd1b6680fc7fcb93c76abc3b452c1655696
AleksejMosunov <136924749+AleksejMosunov@users.noreply.github.com>
2023-08-07 17:11:17 +0300

    Update js.js

--- java/js.js
+++ java/js.js
@@ -1,10 +1,20 @@
-var person = null;
-if(confirm("Если пидор нажми ок"))
-{
-	person = prompt("Назови свое имя");
-	alert("Привет, сер гей " + person)
+const array = ["Лёха Мосунов","Лёша Мосунов","Мосунов Лёха","Мосунов Лёша","Леха Мосунов","Мосунов Леха",
+"Мосунов", "Мосунов Алексей", " Алексей Мосунов"]; 
+
+if(confirm("Если пидор нажми ок")) {
+    const person = prompt("Назови свое имя")
+    const isNameOfLord = array.some((name) => name.toLocaleLowerCase() === person.toLocaleLowerCase())
+    
+    if (isNameOfLord) {
+        alert("Ах ты ж пидор")
+    }   
+
+		else {
+			alert("Привет сер гей "+person)
+		}
+
 }
 
 else {
 	alert("Та ты точно пидор")
-}
+	}
