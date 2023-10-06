30c677f2e84c1255d7827dc2468c09bff76bfadb
AleksejMosunov <136924749+AleksejMosunov@users.noreply.github.com>
2023-10-07 00:33:07 +0300

    Add files via upload

---
+++ react-raketa/src/Click.js
@@ -0,0 +1,6 @@
+function Click() {
+  let popup = document.getElementById('myPopup');
+  popup.classList.toggle('show');
+}
+
+export default Click;
--- react-raketa/src/Team.js
+++ react-raketa/src/Team.js
@@ -23,13 +23,81 @@ function Team() {
   let stintTime = `39:58`;
   let teamNumber = 1;
   let driver = `Valera`;
+
+  function myFunction(e) {
+    e.preventDefault();
+    let popup = document.getElementById('myPopup');
+    popup.classList.toggle('show');
+  }
+
   return (
     <div className="team">
       <p className="team_number pro">{teamNumber}</p>
-      <p className="team_driver driver_raketa">
+      <div className="popup team_driver driver_raketa" onClick={myFunction}>
         {driver}
+        <div className="popuptext" id="myPopup">
+          <ul className="laps">
+            <li>49.99</li>
+            <li>49.87</li>
+            <li>49.67</li>
+            <li>49.99</li>
+            <li>49.87</li>
+            <li>49.67</li>
+            <li>49.99</li>
+            <li>49.87</li>
+            <li>49.67</li>
+            <li>49.99</li>
+            <li>49.87</li>
+            <li>49.67</li>
+            <li>49.99</li>
+            <li>49.87</li>
+            <li>49.67</li>
+            <li>49.99</li>
+            <li>49.87</li>
+            <li>49.67</li>
+            <li>49.99</li>
+            <li>49.87</li>
+            <li>49.67</li>
+            <li>49.99</li>
+            <li>49.87</li>
+            <li>49.67</li>
+            <li>49.99</li>
+            <li>49.87</li>
+            <li>49.67</li>
+            <li>49.99</li>
+            <li>49.87</li>
+            <li>49.67</li>
+            <li>49.99</li>
+            <li>49.87</li>
+            <li>49.67</li>
+            <li>49.99</li>
+            <li>49.87</li>
+            <li>49.67</li>
+            <li>49.99</li>
+            <li>49.87</li>
+            <li>49.67</li>
+            <li>49.99</li>
+            <li>49.87</li>
+            <li>49.67</li>
+            <li>49.99</li>
+            <li>49.87</li>
+            <li>49.67</li>
+            <li>49.99</li>
+            <li>49.87</li>
+            <li>49.67</li>
+            <li>49.99</li>
+            <li>49.87</li>
+            <li>49.67</li>
+            <li>49.99</li>
+            <li>49.87</li>
+            <li>49.67</li>
+            <li>49.87</li>
+            <li>49.67</li>
+          </ul>
+        </div>
+
         {/* <img className="raketa_img" src={raketa} alt="raketa" width="25px" height="25px" /> */}
-      </p>
+      </div>
       <p className="team_avg_time raketa drova">{avgTime.toFixed(2)}</p>
       <p className="team_best_time best">{bestTime.toFixed(2)}</p>
       <p className="team_last_time better worse best">{lastTime.toFixed(2)}</p>
