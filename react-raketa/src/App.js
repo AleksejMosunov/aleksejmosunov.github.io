152940e0ff7eefef7fb9d9e7bc6f39eceec1e9c6
AleksejMosunov <136924749+AleksejMosunov@users.noreply.github.com>
2024-01-01 13:43:50 +0200

    Delete react-raketa directory

--- react-raketa/src/App.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import Team from './Team';
-import { teamInfo } from './utils';
-
-export default function App() {
-  return teamInfo.map((teamData) => <Team teamData={teamData} key={teamData.teamId} />);
-}
--- react-raketa/src/Click.js
+++
@@ -1,6 +0,0 @@
-function Click() {
-  let popup = document.getElementById('myPopup');
-  popup.classList.toggle('show');
-}
-
-export default Click;
